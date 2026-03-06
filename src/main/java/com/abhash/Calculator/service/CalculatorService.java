3801925f3448ac1e3817e331dbe07d10dc36456b
AbhashTi <abhashtiwari12@gmail.com>
2026-03-06 23:27:15 +0530

    updated calcicode

--- src/main/java/com/abhash/Calculator/service/CalculatorService.java
+++ src/main/java/com/abhash/Calculator/service/CalculatorService.java
@@ -5,16 +5,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorService {
 
+    // Square Root
     public double squareRoot(double x) {
+
+        if (x < 0) {
+            throw new IllegalArgumentException("Square root of negative number is not defined");
+        }
+
         return Math.sqrt(x);
     }
 
+    // Factorial
     public long factorial(int n) {
+
         if (n < 0) {
-            throw new IllegalArgumentException("Number must be non-negative");
+            throw new IllegalArgumentException("Factorial of negative number is not defined");
+        }
+
+        if (n > 20) {
+            throw new IllegalArgumentException("Number too large (overflow risk)");
         }
 
         long result = 1;
+
         for(int i = 1; i <= n; i++) {
             result *= i;
         }
@@ -22,15 +35,23 @@ public class CalculatorService {
         return result;
     }
 
+    // Natural Log
     public double naturalLog(double x) {
+
         if (x <= 0) {
-            throw new IllegalArgumentException("Input must be positive");
+            throw new IllegalArgumentException("Natural log undefined for zero or negative numbers");
         }
 
         return Math.log(x);
     }
 
+    // Power Function
     public double power(double base, double exponent) {
+
+        if(base == 0 && exponent == 0) {
+            throw new IllegalArgumentException("0^0 is undefined");
+        }
+
         return Math.pow(base, exponent);
     }
 }
