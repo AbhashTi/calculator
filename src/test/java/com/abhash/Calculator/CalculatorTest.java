ab02399ce366eb0771ee2b54c354b3aabcd5ac0c
AbhashTi <abhashtiwari12@gmail.com>
2026-03-07 08:59:20 +0530

    junit updation

--- src/test/java/com/abhash/Calculator/CalculatorTest.java
+++ src/test/java/com/abhash/Calculator/CalculatorTest.java
@@ -1,9 +1,9 @@
 package com.abhash.Calculator;
 
+import com.abhash.Calculator.service.CalculatorService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import com.abhash.Calculator.service.CalculatorService;
 
 public class CalculatorTest {
 
@@ -15,12 +15,22 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testAdd() {
-        Assertions.assertEquals(3, calculator.add(1,2));
+    public void testSquareRoot() {
+        Assertions.assertEquals(5.0, calculator.squareRoot(25));
     }
 
     @Test
-    public void testSub() {
-        Assertions.assertEquals(1, calculator.sub(3,2));
+    public void testFactorial() {
+        Assertions.assertEquals(120, calculator.factorial(5));
+    }
+
+    @Test
+    public void testNaturalLog() {
+        Assertions.assertEquals(Math.log(10), calculator.naturalLog(10));
+    }
+
+    @Test
+    public void testPower() {
+        Assertions.assertEquals(8.0, calculator.power(2,3));
     }
 }
